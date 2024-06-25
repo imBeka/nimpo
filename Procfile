380487a06331cbdb4e1565a320e19df6d949a694
imBeka <bka05yu@gmail.com>
2024-06-25 15:13:35 +0100

    main py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:  gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 api:app
+web:  gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 main:app
--- api.py
+++ api.py
@@ -113,8 +113,9 @@ def handle_message(data):
         message = data['text']
         send_message_to_operators(chat_id, message, sender_id, chat_name)
 
-# def start_flask_server():
-#     socketio.run(app, host='0.0.0.0', port=8000, debug=True, use_reloader=False)
+def start_flask_server():
+    socketio.run(app, host='0.0.0.0', port=8000, debug=True, use_reloader=False)
+    app.run()
 
 if __name__ == '__main__':
     app.run() 
